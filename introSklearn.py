425fc756fa01e94386032b1d581545798f4bbe0f
pNatzz <p.tueku@gmail.com>
2021-02-24 11:16:50 +0700

    delete file

--- introSklearn.py
+++
@@ -1,9 +0,0 @@
-# sklearn : machine learning library
-import warnings
-warnings.simplefilter('ignore')
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import datasets
-
-iris = datasets.load_iris()
