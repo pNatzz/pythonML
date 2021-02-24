76ca34f3ccd2fc870d2e7bb493d326ce2367aab3
pNatzz <p.tueku@gmail.com>
2021-02-24 09:39:50 +0700

    revised

---
+++ introSklearn.py
@@ -0,0 +1,9 @@
+# sklearn : machine learning library
+import warnings
+warnings.simplefilter('ignore')
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import datasets
+
+iris = datasets.load_iris()
