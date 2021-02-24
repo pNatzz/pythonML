82b0a11cd8865cbc70d5b41e74e55fa7cc9f0f99
pNatzz <p.tueku@gmail.com>
2021-02-24 08:12:06 +0700

    add new line

--- pandasBasic.py
+++ pandasBasic.py
@@ -1,3 +1,5 @@
 import pandas as pd
-df = pd.read_csv("https://github.com/pNatzz/pythonML/blob/master/pm-24-jan-2021.csv")
+df = pd.read_csv("C:/Users/1000219787/pythonML/pm-24-jan-2021.csv")
 print(df.shape)
+print(df.head())
+print(df.tail())
