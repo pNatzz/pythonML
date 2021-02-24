895a66c793bfca84a07487e3c4e2661753c97fc7
pNatzz <p.tueku@gmail.com>
2021-02-24 08:36:53 +0700

    revise

--- pandasBasic.py
+++ pandasBasic.py
@@ -1,3 +1,9 @@
 import pandas as pd
-df = pd.read_csv("C:/Users/1000219787/pythonML/pm-24-jan-2021.csv")
-print(df.shape)
+from pathlib import Path
+
+sourcedir = Path("C:/Users/1000219787/pythonML")
+
+df = pd.read_csv(sourcedir/'pm-24-jan-2021.csv')
+print(df.head())
+print(df.tail())
+headTail = pd.concat([df.head(), df.tail()], ignore_index = True)
